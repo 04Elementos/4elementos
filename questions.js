85e4dfa3dcf283007714fe0cd5b991c4a86857b2
Giovanne Cerione <giovannecerione1@gmail.com>
2021-12-07 19:24:41 -0300

    Começo do Projeto
    Criação do projeto, sistema de trocas de perguntas e páginas iniciais.

---
+++ questions.js
@@ -0,0 +1,16 @@
+questions = [
+    {
+        "id": 1,
+        "question": "Você é de sangue?",
+        "effect": {
+            "sangue": 1,
+        }
+    },
+    {
+        "id": 2,
+        "question": "Você não é de sangue?",
+        "effect": {
+            "morte": 1,
+        }
+    }
+]
