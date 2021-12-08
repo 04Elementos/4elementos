92cb670e41b4691f62edbc4e9eacb59c54581d27
Giovanne Cerione <giovannecerione1@gmail.com>
2021-12-08 19:39:11 -0300

    CSS, Elementos e Organização dos Resultados.
    Criação do arquivos dos elementos, além do CSS da página principal e do teste. O resultado no final é organizado do maior para o menor.

---
+++ elements.js
@@ -0,0 +1,46 @@
+elements = [
+    {
+        "name": "Morte",
+        "desc": "Descrição de Morte",
+        "fraseGuia": "",
+        "espectro": {
+            "emocao": 11,
+            "solidao": -5,
+            "ordem": 1,
+            "caos": 3,
+        }
+    },
+    {
+        "name": "Sangue",
+        "desc": "Descrição de Sangue",
+        "fraseGuia": "",
+        "espectro": {
+            "emocao": -1,
+            "solidao": 11,
+            "ordem": 3,
+            "caos": -5,
+        }
+    },
+    {
+        "name": "Energia",
+        "desc": "Descrição de Energia",
+        "fraseGuia": "",
+        "espectro": {
+            "emocao": 3,
+            "solidao": -1,
+            "ordem": -5,
+            "caos": 11,
+        }
+    },
+    {
+        "name": "Conhecimento",
+        "desc": "Descrição de Conhecimento",
+        "fraseGuia": "",
+        "espectro": {
+            "emocao": -5,
+            "solidao": 3,
+            "ordem": 11,
+            "caos": -1,
+        }
+    },
+]
--- questions.js
+++ questions.js
@@ -1,16 +1,42 @@
 questions = [
     {
         "id": 1,
-        "question": "Você é de sangue?",
+        "question": "Sangue",
         "effect": {
-            "sangue": 1,
+            "emocao": 2,
+            "solidao": -2,
+            "ordem": -1,
+            "caos": 0,
         }
     },
     {
         "id": 2,
-        "question": "Você não é de sangue?",
+        "question": "Morte",
         "effect": {
-            "morte": 1,
+            "emocao": -1,
+            "solidao": 2,
+            "ordem": 0,
+            "caos": -2,
         }
-    }
+    },
+    {
+        "id": 3,
+        "question": "Conhecimento",
+        "effect": {
+            "emocao": -2,
+            "solidao": 0,
+            "ordem": 2,
+            "caos": -1,
+        }
+    },
+    {
+        "id": 4,
+        "question": "Energia",
+        "effect": {
+            "emocao": 0,
+            "solidao": -1,
+            "ordem": -2,
+            "caos": 2,
+        }
+    },
 ]
