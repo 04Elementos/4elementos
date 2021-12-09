a3d80546f2fc0a2b4de07727e73dd08902f5bacd
Giovanne Cerione <giovannecerione1@gmail.com>
2021-12-09 00:58:05 -0300

    Porcentagem dos resultados

--- elements.js
+++ elements.js
@@ -3,44 +3,24 @@ elements = [
         "name": "Morte",
         "desc": "Descrição de Morte",
         "fraseGuia": "",
-        "espectro": {
-            "emocao": 11,
-            "solidao": -5,
-            "ordem": 1,
-            "caos": 3,
-        }
+        "espectro": [40, 100, 60, 20]
     },
     {
         "name": "Sangue",
         "desc": "Descrição de Sangue",
         "fraseGuia": "",
-        "espectro": {
-            "emocao": -1,
-            "solidao": 11,
-            "ordem": 3,
-            "caos": -5,
-        }
+        "espectro": [100, 20, 40, 60]
     },
     {
         "name": "Energia",
         "desc": "Descrição de Energia",
         "fraseGuia": "",
-        "espectro": {
-            "emocao": 3,
-            "solidao": -1,
-            "ordem": -5,
-            "caos": 11,
-        }
+        "espectro": [60, 40, 20, 100]
     },
     {
         "name": "Conhecimento",
         "desc": "Descrição de Conhecimento",
         "fraseGuia": "",
-        "espectro": {
-            "emocao": -5,
-            "solidao": 3,
-            "ordem": 11,
-            "caos": -1,
-        }
+        "espectro": [20, 60, 100, 40]
     },
 ]
