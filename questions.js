1288983846e5b6b458dc9b43b209585cf906ddea
Giovanne Cerione <giovannecerione1@gmail.com>
2021-12-12 17:08:54 -0300

    Adicionado novas perguntas (4/20)

--- questions.js
+++ questions.js
@@ -1,42 +1,42 @@
 questions = [
     {
         "id": 1,
-        "question": "Sangue",
+        "question": "Você permanece relaxado e concentrado mesmo sobre pressão.",
         "effect": {
-            "emocao": 2,
-            "solidao": -2,
-            "ordem": -1,
-            "caos": 0,
-        }
-    },
-    {
-        "id": 2,
-        "question": "Morte",
-        "effect": {
-            "emocao": -1,
-            "solidao": 2,
-            "ordem": 0,
+            "emocao": 0,
+            "solidao": 0,
+            "ordem": 2,
             "caos": -2,
         }
     },
     {
-        "id": 3,
-        "question": "Conhecimento",
+        "id": 2,
+        "question": "Você normalmente faz as coisas sem pensar.",
         "effect": {
-            "emocao": -2,
-            "solidao": 0,
-            "ordem": 2,
-            "caos": -1,
+            "emocao": 0,
+            "solidao": -2,
+            "ordem": 0,
+            "caos": 2,
+        }
+    },
+    {
+        "id": 3,
+        "question": "Você deixa seus sentimentos ditarem suas ações.",
+        "effect": {
+            "emocao": 2,
+            "solidao": -2,
+            "ordem": 0,
+            "caos": 0,
         }
     },
     {
         "id": 4,
-        "question": "Energia",
+        "question": "Você baseia suas ações por seus impactos no presente, não no futuro.",
         "effect": {
-            "emocao": 0,
-            "solidao": -1,
-            "ordem": -2,
-            "caos": 2,
+            "emocao": -2,
+            "solidao": 2,
+            "ordem": 0,
+            "caos": 0,
         }
     },
 ]
