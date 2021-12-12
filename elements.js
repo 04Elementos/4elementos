2f03d400d09a9dfd0ba008d8c8855e8b64bedf1d
Giovanne Cerione <giovannecerione1@gmail.com>
2021-12-12 14:25:39 -0300

    Twitter meta tags, descrição dos elementos

--- elements.js
+++ elements.js
@@ -1,21 +1,21 @@
 elements = {
     "Morte": {
-        "desc": "Lord Ipsum duis rhoncus leo enim, quis ullamcorper neque consectetur vestibulum. Cras placerat eros in finibus ultricies. Aliquam sagittis id nunc et varius. Morbi pellentesque tincidunt est, ultricies rhoncus leo suscipit eu. Nullam sapien purus, laoreet eu dui et, accumsan hendrerit leo. Sed volutpat tristique augue nec posuere. <br><br>Praesent dapibus feugiat orci, in ornare neque facilisis nec. Duis auctor congue ullamcorper.Duis sit amet ex nisi. In tempus facilisis nunc, et luctus est venenatis eu. Mauris tincidunt nec augue vitae faucibus. Suspendisse eu mi egestas, facilisis mi sed, efficitur sapien. Etiam convallis vestibulum vulputate. In at finibus augue. Phasellus sagittis tortor eu molestie sodales. Phasellus diam turpis, sollicitudin non scelerisque non, posuere varius magna. Suspendisse et maximus neque. Cras sapien mauris, consectetur at metus in, sollicitudin ultricies dolor.",
+        "desc": "Morte é a entidade do tempo. Ela busca os momentos vivenciados, distorcendo a percepção egóica da existência de cada indivíduo para seu próprio agrado.<br><br>A distorção temporal da Morte arruína a percepção carnal do Sangue.",
         "fraseGuia": "Tudo tem um começo e um fim, e o Tempo leva todas as coisas. Nada que é levado pela Morte pode voltar ao que era antes.",
     },
 
     "Sangue": {
-        "desc": "Descrição de Sangue",
+        "desc": "Sangue é a entidade do sentimento. Ele busca a intensidade: dor, obsessão, paixão, amor, fome, ódio - tudo que envolve sentir uma emoção extrema agrada a entidade de Sangue.<br><br>Os sentimentos extremos do Sangue superam a razão e a calmaria do Conhecimento.",
         "fraseGuia": "Tudo começa pelo Sangue. O Sangue é o fluxo que banha a eternidade do Outro Lado",
     },
 
     "Energia": {
-        "desc": "Descrição de Energia",
+        "desc": "Energia é a entidade do caos. Tudo que não pode ser explicado, o intangível, a anarquia. A constante mudança, o calor e o frio, a luz e as trevas. Tudo que envolve a imprevisibilidade e a transformação agrada a entidade de Energia.<br><br>A transformação da Energia sobrecarrega os efeitos da Morte",
         "fraseGuia": "O Caos é inevitável.",
     },
 
     "Conhecimento": {
-        "desc": "Descrição de Conhecimento",
+        "desc": "Conhecimento é a entidade da consciência. Descobrir, aprender, conhecer, decifrar. Ter a própria percepção do Outro Lado e suas entidades agrada o elemento de Conhecimento.<br><br>A razão e lógica do Conhecimento reintegram e suprimem o caos da Energia",
         "fraseGuia": "Saber tudo é perder tudo.",
     }
 }
