f185ca8e507c6ca36d24f160c12090c209df4e26
Giovanne Cerione <giovannecerione1@gmail.com>
2021-12-14 19:27:08 -0300

    Adicionada mais perguntas (20/20)

--- questions.js
+++ questions.js
@@ -4,7 +4,7 @@ questions = [
         "question": "Você permanece relaxado e concentrado mesmo sobre pressão.",
         "effect": {
             "emocao": 0,
-            "solidao": 0,
+            "solidao": 1,
             "ordem": 2,
             "caos": -2,
         }
@@ -25,8 +25,8 @@ questions = [
         "effect": {
             "emocao": 2,
             "solidao": -2,
-            "ordem": 0,
-            "caos": 0,
+            "ordem": -1,
+            "caos": 1,
         }
     },
     {
@@ -35,8 +35,168 @@ questions = [
         "effect": {
             "emocao": -2,
             "solidao": 2,
-            "ordem": 0,
+            "ordem": -1,
+            "caos": 1,
+        }
+    },
+    {
+        "id": 5,
+        "question": "Você prefere realizar atividades em grupo.",
+        "effect": {
+            "emocao": 0,
+            "solidao": -2,
+            "ordem": 1,
             "caos": 0,
+        }
+    },
+    {
+        "id": 6,
+        "question": "Você tende a liderar as atividades.",
+        "effect": {
+            "emocao": 2,
+            "solidao": -2,
+            "ordem": 1,
+            "caos": -1,
+        }
+    },
+    {
+        "id": 7,
+        "question": "As emoções dos outros pouco importam pra você.",
+        "effect": {
+            "emocao": -2,
+            "solidao": 0,
+            "ordem": 2,
+            "caos": 1,
+        }
+    },
+    {
+        "id": 8,
+        "question": "Lealdade é um dos pilares mais importantes de um grupo.",
+        "effect": {
+            "emocao": 2,
+            "solidao": -1,
+            "ordem": 1,
+            "caos": -2,
+        }
+    },
+    {
+        "id": 9,
+        "question": "Sua vontade própria supera a vontade de um grupo como um todo.",
+        "effect": {
+            "emocao": 0,
+            "solidao": 1,
+            "ordem": -1,
+            "caos": 2,
+        }
+    },
+    {
+        "id": 10,
+        "question": "A verdade é mais importante que os sentimentos do grupo.",
+        "effect": {
+            "emocao": -2,
+            "solidao": 1,
+            "ordem": 2,
+            "caos": -1,
+        }
+    },
+    {
+        "id": 11,
+        "question": "Ser um líder é mais respeitavél que estar na liderança.",
+        "effect": {
+            "emocao": 2,
+            "solidao": -1,
+            "ordem": -2,
+            "caos": 1,
+        }
+    },
+    {
+        "id": 12,
+        "question": "Planejamento não é a parte mais importante de uma missão.",
+        "effect": {
+            "emocao": 0,
+            "solidao": 0,
+            "ordem": -2,
+            "caos": 2,
+        }
+    },
+    {
+        "id": 13,
+        "question": "Não existem inocentes do lado do inimigo, todos devem ser julgados igualmente.",
+        "effect": {
+            "emocao": 2,
+            "solidao": -1,
+            "ordem": -2,
+            "caos": 1,
+        }
+    },
+    {
+        "id": 14,
+        "question": "Pessoas que são menos efetivas que você te estressam.",
+        "effect": {
+            "emocao": -2,
+            "solidao": 2,
+            "ordem": 1,
+            "caos": -1,
+        }
+    },
+    {
+        "id": 15,
+        "question": "Você não se importa em como suas ações afetam alguém.",
+        "effect": {
+            "emocao": -2,
+            "solidao": 1,
+            "ordem": -1,
+            "caos": 2,
+        }
+    },
+    {
+        "id": 16,
+        "question": "Você acredita que deve sempre se justificar.",
+        "effect": {
+            "emocao": 2,
+            "solidao": -1,
+            "ordem": 1,
+            "caos": -2,
+        }
+    },
+    {
+        "id": 17,
+        "question": "Você tem dificuldade em se adaptar a novos grupos.",
+        "effect": {
+            "emocao": -1,
+            "solidao": 2,
+            "ordem": -1,
+            "caos": 0,
+        }
+    },
+    {
+        "id": 18,
+        "question": "Você é mais prático do que criativo.",
+        "effect": {
+            "emocao": 0,
+            "solidao": 1,
+            "ordem": 2,
+            "caos": 0,
+        }
+    },
+    {
+        "id": 19,
+        "question": "Se o resultado for positivo, pouco importa o meio.",
+        "effect": {
+            "emocao": -1,
+            "solidao": 1,
+            "ordem": 0,
+            "caos": 2,
+        }
+    },
+    {
+        "id": 20,
+        "question": "Você tem facilidade em adaptar seus planos.",
+        "effect": {
+            "emocao": 1,
+            "solidao": 1,
+            "ordem": -1,
+            "caos": 1,
         }
     },
 ]
