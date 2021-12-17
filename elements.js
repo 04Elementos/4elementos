e1c6a984c260a927677c22976166b3bb48f639d0
Giovanne Cerione <giovannecerione1@gmail.com>
2021-12-17 13:42:52 -0300

    Sessão de outras informações
    Criada sessão de outras informações relacionadas ao seu resultado.

--- elements.js
+++ elements.js
@@ -2,25 +2,60 @@ elements = {
     "Morte": {
         "desc": "Morte é a entidade do tempo. Ela busca os momentos vivenciados, distorcendo a percepção egóica da existência de cada indivíduo para seu próprio agrado.<br><br>A distorção temporal da Morte arruína a percepção carnal do Sangue.",
         "fraseGuia": "Tudo tem um começo e um fim, e o Tempo leva todas as coisas. Nada que é levado pela Morte pode voltar ao que era antes.",
+        "outrasInfo": {
+            "agts": {
+                "agt_1": "<a>Dante</a>, cujo nome original é Gaspar, é um dos protagonistas da série Ordem Paranormal, presente em Desconjuração e Calamidade. Ele era um ocultista que foi preso por sua participação em um ritual envolvendo Luciano Carvalho.",
+                "agt_2": "<a>Boris Lukic</a> foi um dos protagonistas da série Ordem Paranormal, presente em Calamidade. Ele era um dos Marcados, anteriormente, do lado de Kian, trabalhando junto de outros Escriptas por conta de um acordo que fez com Kian envolvendo o próprio Diabo.",
+                "agt_3": "<a>GuaGuazinho</a>, o <a href='https://twitter.com/GuaxinimGamer/status/1471619907167174660?s=20'>Guaxinim</a> (Interpreta o personagem Arthur Cervero) realizou nosso teste e seu resultado foi Morte.",
+            }
+        }
     },
 
     "Sangue": {
         "desc": "Sangue é a entidade do sentimento. Ele busca a intensidade: dor, obsessão, paixão, amor, fome, ódio - tudo que envolve sentir uma emoção extrema agrada a entidade de Sangue.<br><br>Os sentimentos extremos do Sangue superam a razão e a calmaria do Conhecimento.",
         "fraseGuia": "Tudo começa pelo Sangue. O Sangue é o fluxo que banha a eternidade do Outro Lado",
+        "outrasInfo": {
+            "agts": {
+                "agt_1": "",
+                "agt_2": "",
+                "agt_3": "",
+            }
+        }
     },
 
     "Energia": {
         "desc": "Energia é a entidade do caos. Tudo que não pode ser explicado, o intangível, a anarquia. A constante mudança, o calor e o frio, a luz e as trevas. Tudo que envolve a imprevisibilidade e a transformação agrada a entidade de Energia.<br><br>A transformação da Energia sobrecarrega os efeitos da Morte",
         "fraseGuia": "O Caos é inevitável.",
+        "outrasInfo": {
+            "agts": {
+                "agt_1": "",
+                "agt_2": "",
+                "agt_3": "",
+            }
+        }
     },
 
     "Conhecimento": {
         "desc": "Conhecimento é a entidade da consciência. Descobrir, aprender, conhecer, decifrar. Ter a própria percepção do Outro Lado e suas entidades agrada o elemento de Conhecimento.<br><br>A razão e lógica do Conhecimento reintegram e suprimem o caos da Energia",
         "fraseGuia": "Saber tudo é perder tudo.",
+        "outrasInfo": {
+            "agts": {
+                "agt_1": "",
+                "agt_2": "",
+                "agt_3": "",
+            }
+        }
     },
 
     "Medo": {
         "desc": "Você ouve uma música impossível, lê a descrição do elemento, mas você não entende o que leu. O Medo é impossível de entender. Seus 4 espectros são únicos, assim como o Medo.",
-        "fraseGuia": "O Medo é infinito."
+        "fraseGuia": "O Medo é infinito.",
+        "outrasInfo": {
+            "agts": {
+                "agt_1": "",
+                "agt_2": "",
+                "agt_3": "",
+            }
+        }
     }
 }
