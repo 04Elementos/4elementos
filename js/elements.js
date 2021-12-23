13fbac904a2c097e97f487c1108154dcd23f0ca4
Giovanne Cerione <giovannecerione1@gmail.com>
2021-12-23 14:50:50 -0300

    Personalização dos resultados

--- js/elements.js
+++ js/elements.js
@@ -1,6 +1,6 @@
 elements = {
     "Morte": {
-        "desc": "Morte é a entidade do tempo. Ela busca os momentos vivenciados, distorcendo a percepção egóica da existência de cada indivíduo para seu próprio agrado.<br><br>A distorção temporal da Morte arruína a percepção carnal do Sangue.",
+        "desc": "<span>Morte é a entidade do tempo</span>. Ela busca os momentos vivenciados, distorcendo a percepção egóica da existência de cada indivíduo para seu próprio agrado. <span>A distorção temporal da Morte arruína a percepção carnal do Sangue</span>.",
         "fraseGuia": "Tudo tem um começo e um fim, e o Tempo leva todas as coisas. Nada que é levado pela Morte pode voltar ao que era antes.",
         "outrasInfo": {
             "agts": {
@@ -12,7 +12,7 @@ elements = {
     },
 
     "Sangue": {
-        "desc": "Sangue é a entidade do sentimento. Ele busca a intensidade: dor, obsessão, paixão, amor, fome, ódio - tudo que envolve sentir uma emoção extrema agrada a entidade de Sangue.<br><br>Os sentimentos extremos do Sangue superam a razão e a calmaria do Conhecimento.",
+        "desc": "<span>Sangue é a entidade do sentimento</span>. Ele busca a intensidade: dor, obsessão, paixão, amor, fome, ódio - tudo que envolve sentir uma emoção extrema agrada a entidade de Sangue. <span>Os sentimentos extremos do Sangue superam a razão e a calmaria do Conhecimento</span>.",
         "fraseGuia": "Tudo começa pelo Sangue. O Sangue é o fluxo que banha a eternidade do Outro Lado",
         "outrasInfo": {
             "agts": {
@@ -24,7 +24,7 @@ elements = {
     },
 
     "Energia": {
-        "desc": "Energia é a entidade do caos. Tudo que não pode ser explicado, o intangível, a anarquia. A constante mudança, o calor e o frio, a luz e as trevas. Tudo que envolve a imprevisibilidade e a transformação agrada a entidade de Energia.<br><br>A transformação da Energia sobrecarrega os efeitos da Morte",
+        "desc": "<span>Energia é a entidade do caos</span>. Tudo que não pode ser explicado, o intangível, a anarquia. A constante mudança, o calor e o frio, a luz e as trevas. Tudo que envolve a imprevisibilidade e a transformação agrada a entidade de Energia. <span>A transformação da Energia sobrecarrega os efeitos da Morte</span>.",
         "fraseGuia": "O Caos é inevitável.",
         "outrasInfo": {
             "agts": {
@@ -36,7 +36,7 @@ elements = {
     },
 
     "Conhecimento": {
-        "desc": "Conhecimento é a entidade da consciência. Descobrir, aprender, conhecer, decifrar. Ter a própria percepção do Outro Lado e suas entidades agrada o elemento de Conhecimento.<br><br>A razão e lógica do Conhecimento reintegram e suprimem o caos da Energia",
+        "desc": "<span>Conhecimento é a entidade da consciência</span>. Descobrir, aprender, conhecer, decifrar. Ter a própria percepção do Outro Lado e suas entidades agrada o elemento de Conhecimento. <span>A razão e lógica do Conhecimento reintegram e suprimem o caos da Energia</span>.",
         "fraseGuia": "Saber tudo é perder tudo.",
         "outrasInfo": {
             "agts": {
