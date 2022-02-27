f40c60aa7201a9e4a5eed4d800a738c18712df1c
Giovanne Cerione <giovannecerione1@gmail.com>
2022-02-27 12:50:34 -0300

    Wordle Paranormal
    Criação do minigame wordle paranormal

---
+++ js/dailyword.js
@@ -0,0 +1,7 @@
+dailyWords = {
+    '2022-02-27': { edition: '001', date: '2022-02-27', word: 'MORTE' },
+    '2022-02-28': { edition: '002', date: '2022-02-28', word: 'LEONE' },
+    '2022-03-01': { edition: '003', date: '2022-03-01', word: 'SANTO' },
+    '2022-03-02': { edition: '004', date: '2022-03-02', word: 'DAMIR' },
+    '2022-03-03': { edition: '005', date: '2022-03-03', word: 'BORIS' },
+}
---
+++ js/wordle.js
@@ -0,0 +1,190 @@
+const tileDisplay = document.querySelector('.tile-container')
+const keyboard = document.querySelector('.key-container')
+const messageDisplay = document.querySelector('.message-container')
+
+// Code to get DailyWord by Gabriel Toschi and Gabriel Kanegae @ Letreco
+const milissecondsMinute = 60 * 1000;
+
+function getToday() {
+    const today = new Date();
+    const correctedDate = new Date(
+        today.valueOf() - (today.getTimezoneOffset() * milissecondsMinute)
+    )
+
+    return correctedDate.toISOString().split('T')[0]
+}
+
+function getDailyWord() {
+    return dailyWords[getToday()]
+}
+
+const wordle = getDailyWord().word
+
+const keys = [
+    'Q',
+    'W',
+    'E',
+    'R',
+    'T',
+    'Y',
+    'U',
+    'I',
+    'O',
+    'P',
+    'A',
+    'S',
+    'D',
+    'F',
+    'G',
+    'H',
+    'J',
+    'K',
+    'L',
+    'ENTER',
+    'Z',
+    'X',
+    'C',
+    'V',
+    'B',
+    'N',
+    'M',
+    'DEL',
+]
+
+const guessRows = [
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', '']
+]
+
+let currentRow = 0;
+let currentTile = 0;
+let isGameOver = false;
+
+guessRows.forEach((guessRow, guessRowIndex) => {
+    const rowElement = document.createElement('div');
+    rowElement.setAttribute('id', 'guessRow-' + guessRowIndex)
+
+    guessRow.forEach((guess, guessIndex) => {
+        const tileElement = document.createElement('div');
+        tileElement.setAttribute('id', 'guessRow-' + guessRowIndex + '-tile-' + guessIndex);
+        tileElement.classList.add('tile')
+        rowElement.append(tileElement);
+    })
+
+    tileDisplay.append(rowElement);
+})
+
+keys.forEach(key => {
+    const buttonElement = document.createElement('button');
+    buttonElement.textContent = key;
+    buttonElement.setAttribute('id', key);
+    buttonElement.addEventListener('click', () => handleClick(key));
+    keyboard.append(buttonElement);
+});
+
+const handleClick = (key) => {
+    if (key === 'DEL') {
+        deleteLetter()
+        return
+    }
+
+    if (key === 'ENTER') {
+        checkRow()
+        return
+    }
+
+    addLetter(key)
+}
+
+const addLetter = (letter) => {
+    if(currentTile < 5 && currentRow < 6) {
+        const tile = document.getElementById(`guessRow-${currentRow}-tile-${currentTile}`);
+        tile.textContent = letter;
+        guessRows[currentRow][currentTile] = letter;
+        tile.setAttribute('data', letter)
+    
+        currentTile++
+    }
+}
+
+const deleteLetter = () => {
+    if(currentTile > 0) {
+        currentTile--
+        const tile = document.getElementById(`guessRow-${currentRow}-tile-${currentTile}`);
+        tile.textContent = ''
+        guessRows[currentRow][currentTile] = '';
+        tile.setAttribute('data', '')
+    }
+}
+
+const checkRow = () => {
+    const guess = guessRows[currentRow].join('')
+    flipTile()
+
+    if (currentTile === 5) {
+        if(wordle === guess) {
+            showMessage('Yay!')
+            isGameOver = true;
+            return;
+        } 
+        else {
+            if(currentRow >= 5) {
+                isGameOver = true;
+                showMessage('nah')
+            }
+
+            if(currentRow < 5) {
+                currentRow++
+                currentTile = 0;
+            }
+        }
+    }
+}
+
+const showMessage = (message) => {
+    const messageElement = document.createElement('p')
+    messageElement.textContent = message;
+    messageDisplay.append(messageElement)
+}
+
+const addColorToKey = (keyLetter, color) => {
+    const key = document.getElementById(keyLetter)
+    key.classList.add(color)
+}
+
+const flipTile = () => {
+    const rowTiles = document.querySelector('#guessRow-' + currentRow).childNodes
+    let checkWordle = wordle
+    const guess = []
+
+    rowTiles.forEach(tile => {
+        guess.push({letter: tile.getAttribute('data'), color: 'grey-overlay'})
+    })
+
+    guess.forEach((guess, index) => {
+        if (guess.letter == wordle[index]) {
+            guess.color = 'green-overlay'
+            checkWordle = checkWordle.replace(guess.letter, '')
+        }
+    })
+
+    guess.forEach(guess => {
+        // Code by Ram Prasad
+        if ((checkWordle.includes(guess.letter)) && (guess.color != 'green-overlay')){
+            guess.color = 'yellow-overlay'
+            checkWordle = checkWordle.replace(guess.letter, '')
+        }
+    })
+
+    rowTiles.forEach((tile, index) => {
+        setTimeout(() => {
+            tile.classList.add('flip')
+            tile.classList.add(guess[index].color)
+            addColorToKey(guess[index].letter, guess[index].color)
+        }, 500 * index)
+    })
+}
